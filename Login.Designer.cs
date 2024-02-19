d1b8487ee28df8c074ab3566b7533e3635197107
Quốc Bảo Nguyễn <nguyenquocbaopl321@gmail.com>
2024-02-19 18:52:46 +0700

    Add login form

---
+++ Login.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Quan_Li_Luan_Van
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dangnhap_bt = new System.Windows.Forms.Button();
+            this.matkhau_tb = new System.Windows.Forms.TextBox();
+            this.taikhoan_tb = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(296, 328);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dangnhap_bt);
+            this.groupBox2.Controls.Add(this.matkhau_tb);
+            this.groupBox2.Controls.Add(this.taikhoan_tb);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(365, 115);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(388, 263);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin đăng nhập";
+            // 
+            // dangnhap_bt
+            // 
+            this.dangnhap_bt.Location = new System.Drawing.Point(93, 158);
+            this.dangnhap_bt.Name = "dangnhap_bt";
+            this.dangnhap_bt.Size = new System.Drawing.Size(115, 40);
+            this.dangnhap_bt.TabIndex = 5;
+            this.dangnhap_bt.Text = "Đăng nhập";
+            this.dangnhap_bt.UseVisualStyleBackColor = true;
+            // 
+            // matkhau_tb
+            // 
+            this.matkhau_tb.Location = new System.Drawing.Point(93, 104);
+            this.matkhau_tb.Name = "matkhau_tb";
+            this.matkhau_tb.Size = new System.Drawing.Size(273, 26);
+            this.matkhau_tb.TabIndex = 4;
+            // 
+            // taikhoan_tb
+            // 
+            this.taikhoan_tb.Location = new System.Drawing.Point(93, 52);
+            this.taikhoan_tb.Name = "taikhoan_tb";
+            this.taikhoan_tb.Size = new System.Drawing.Size(273, 26);
+            this.taikhoan_tb.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(251, 158);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Thoát";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tài khoản";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Login";
+            this.Text = "Dang Nhap";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button dangnhap_bt;
+        private System.Windows.Forms.TextBox matkhau_tb;
+        private System.Windows.Forms.TextBox taikhoan_tb;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
