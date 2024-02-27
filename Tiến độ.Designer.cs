2ca098b074210e083da65e7a742334f9028fee0d
Chiến Phạm <22110116@student.hcmute.edu.vn>
2024-02-27 15:12:40 +0700

    sửa form tiến độ

--- Tiến độ.Designer.cs
+++ Tiến độ.Designer.cs
@@ -43,57 +43,62 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(166, 111);
+            this.progressBar1.Location = new System.Drawing.Point(187, 193);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(421, 23);
+            this.progressBar1.Size = new System.Drawing.Size(474, 29);
             this.progressBar1.TabIndex = 0;
             // 
             // lblTiendo
             // 
             this.lblTiendo.AutoSize = true;
-            this.lblTiendo.Location = new System.Drawing.Point(18, 118);
+            this.lblTiendo.Location = new System.Drawing.Point(20, 202);
             this.lblTiendo.Name = "lblTiendo";
-            this.lblTiendo.Size = new System.Drawing.Size(103, 16);
+            this.lblTiendo.Size = new System.Drawing.Size(125, 20);
             this.lblTiendo.TabIndex = 1;
             this.lblTiendo.Text = "Đánh giá tiến độ";
             // 
             // lblLuanvan
             // 
             this.lblLuanvan.AutoSize = true;
-            this.lblLuanvan.Location = new System.Drawing.Point(18, 56);
+            this.lblLuanvan.Location = new System.Drawing.Point(20, 57);
             this.lblLuanvan.Name = "lblLuanvan";
-            this.lblLuanvan.Size = new System.Drawing.Size(84, 16);
+            this.lblLuanvan.Size = new System.Drawing.Size(99, 20);
             this.lblLuanvan.TabIndex = 2;
             this.lblLuanvan.Text = "Tên luận văn";
             // 
             // TxtTenluanvan
             // 
-            this.TxtTenluanvan.Location = new System.Drawing.Point(166, 53);
+            this.TxtTenluanvan.Location = new System.Drawing.Point(187, 53);
+            this.TxtTenluanvan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TxtTenluanvan.Name = "TxtTenluanvan";
-            this.TxtTenluanvan.Size = new System.Drawing.Size(421, 22);
+            this.TxtTenluanvan.ReadOnly = true;
+            this.TxtTenluanvan.Size = new System.Drawing.Size(473, 26);
             this.TxtTenluanvan.TabIndex = 3;
             // 
             // lblThoigiancl
             // 
             this.lblThoigiancl.AutoSize = true;
-            this.lblThoigiancl.Location = new System.Drawing.Point(489, 9);
+            this.lblThoigiancl.Location = new System.Drawing.Point(684, 22);
             this.lblThoigiancl.Name = "lblThoigiancl";
-            this.lblThoigiancl.Size = new System.Drawing.Size(105, 16);
+            this.lblThoigiancl.Size = new System.Drawing.Size(122, 20);
             this.lblThoigiancl.TabIndex = 6;
             this.lblThoigiancl.Text = "Thời gian còn lại";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(609, 3);
+            this.textBox1.Location = new System.Drawing.Point(677, 57);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(115, 22);
+            this.textBox1.Size = new System.Drawing.Size(129, 26);
             this.textBox1.TabIndex = 7;
             // 
             // btnHoanthanh
             // 
-            this.btnHoanthanh.Location = new System.Drawing.Point(305, 415);
+            this.btnHoanthanh.Location = new System.Drawing.Point(636, 522);
+            this.btnHoanthanh.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnHoanthanh.Name = "btnHoanthanh";
-            this.btnHoanthanh.Size = new System.Drawing.Size(118, 31);
+            this.btnHoanthanh.Size = new System.Drawing.Size(133, 39);
             this.btnHoanthanh.TabIndex = 8;
             this.btnHoanthanh.Text = "Hoàn thành";
             this.btnHoanthanh.UseVisualStyleBackColor = true;
@@ -101,41 +106,44 @@
             // lblEmailgv
             // 
             this.lblEmailgv.AutoSize = true;
-            this.lblEmailgv.Location = new System.Drawing.Point(18, 172);
+            this.lblEmailgv.Location = new System.Drawing.Point(20, 121);
             this.lblEmailgv.Name = "lblEmailgv";
-            this.lblEmailgv.Size = new System.Drawing.Size(108, 16);
+            this.lblEmailgv.Size = new System.Drawing.Size(127, 20);
             this.lblEmailgv.TabIndex = 9;
             this.lblEmailgv.Text = "Email Giảng viên";
             // 
             // lblMota
             // 
             this.lblMota.AutoSize = true;
-            this.lblMota.Location = new System.Drawing.Point(19, 229);
+            this.lblMota.Location = new System.Drawing.Point(20, 273);
             this.lblMota.Name = "lblMota";
-            this.lblMota.Size = new System.Drawing.Size(83, 16);
+            this.lblMota.Size = new System.Drawing.Size(101, 20);
             this.lblMota.TabIndex = 10;
             this.lblMota.Text = "Mô tả tiến độ";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(166, 172);
+            this.textBox2.Location = new System.Drawing.Point(187, 121);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(421, 22);
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(473, 26);
             this.textBox2.TabIndex = 11;
             // 
             // rTxtMota
             // 
-            this.rTxtMota.Location = new System.Drawing.Point(166, 216);
+            this.rTxtMota.Location = new System.Drawing.Point(187, 270);
+            this.rTxtMota.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.rTxtMota.Name = "rTxtMota";
-            this.rTxtMota.Size = new System.Drawing.Size(421, 181);
+            this.rTxtMota.Size = new System.Drawing.Size(473, 225);
             this.rTxtMota.TabIndex = 12;
             this.rTxtMota.Text = "";
             // 
             // Tiến_độ
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 467);
+            this.ClientSize = new System.Drawing.Size(818, 584);
             this.Controls.Add(this.rTxtMota);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.lblMota);
@@ -147,6 +155,7 @@
             this.Controls.Add(this.lblLuanvan);
             this.Controls.Add(this.lblTiendo);
             this.Controls.Add(this.progressBar1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Tiến_độ";
             this.Text = "Tiến_độ";
             this.ResumeLayout(false);
