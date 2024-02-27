d93cc1e511756bb60bc81a6cb044c51c96ff09ab
HaiAnh251 <149868011+HaiAnh251@users.noreply.github.com>
2024-02-27 08:23:10 +0700

    Sửa tên radio button

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -133,10 +133,10 @@
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(22, 159);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(80, 20);
+            this.radioButton1.Size = new System.Drawing.Size(82, 20);
             this.radioButton1.TabIndex = 6;
             this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Học sinh";
+            this.radioButton1.Text = "Sinh viên";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
@@ -144,10 +144,10 @@
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(207, 159);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 20);
+            this.radioButton2.Size = new System.Drawing.Size(92, 20);
             this.radioButton2.TabIndex = 7;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Giáo viên";
+            this.radioButton2.Text = "Giảng viên";
             this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Login
