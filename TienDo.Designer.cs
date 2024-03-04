3d5bf45526480ef6d23e4314cf9f86843e3caf20
Chiến Phạm <22110116@student.hcmute.edu.vn>
2024-03-04 14:40:50 +0700

    test

---
+++ TienDo.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Quan_Li_Luan_Van
+{
+    partial class FTienDo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblTiendo = new System.Windows.Forms.Label();
+            this.lblLuanvan = new System.Windows.Forms.Label();
+            this.TxtTenluanvan = new System.Windows.Forms.TextBox();
+            this.lblThoigiancl = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnHoanthanh = new System.Windows.Forms.Button();
+            this.lblEmailgv = new System.Windows.Forms.Label();
+            this.lblMota = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.rTxtMota = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(187, 193);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(474, 29);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // lblTiendo
+            // 
+            this.lblTiendo.AutoSize = true;
+            this.lblTiendo.Location = new System.Drawing.Point(20, 202);
+            this.lblTiendo.Name = "lblTiendo";
+            this.lblTiendo.Size = new System.Drawing.Size(125, 20);
+            this.lblTiendo.TabIndex = 1;
+            this.lblTiendo.Text = "Đánh giá tiến độ";
+            // 
+            // lblLuanvan
+            // 
+            this.lblLuanvan.AutoSize = true;
+            this.lblLuanvan.Location = new System.Drawing.Point(20, 57);
+            this.lblLuanvan.Name = "lblLuanvan";
+            this.lblLuanvan.Size = new System.Drawing.Size(99, 20);
+            this.lblLuanvan.TabIndex = 2;
+            this.lblLuanvan.Text = "Tên luận văn";
+            // 
+            // TxtTenluanvan
+            // 
+            this.TxtTenluanvan.Location = new System.Drawing.Point(187, 53);
+            this.TxtTenluanvan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TxtTenluanvan.Name = "TxtTenluanvan";
+            this.TxtTenluanvan.ReadOnly = true;
+            this.TxtTenluanvan.Size = new System.Drawing.Size(473, 26);
+            this.TxtTenluanvan.TabIndex = 3;
+            // 
+            // lblThoigiancl
+            // 
+            this.lblThoigiancl.AutoSize = true;
+            this.lblThoigiancl.Location = new System.Drawing.Point(684, 22);
+            this.lblThoigiancl.Name = "lblThoigiancl";
+            this.lblThoigiancl.Size = new System.Drawing.Size(122, 20);
+            this.lblThoigiancl.TabIndex = 6;
+            this.lblThoigiancl.Text = "Thời gian còn lại";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(677, 57);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 26);
+            this.textBox1.TabIndex = 7;
+            // 
+            // btnHoanthanh
+            // 
+            this.btnHoanthanh.Location = new System.Drawing.Point(636, 522);
+            this.btnHoanthanh.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnHoanthanh.Name = "btnHoanthanh";
+            this.btnHoanthanh.Size = new System.Drawing.Size(133, 39);
+            this.btnHoanthanh.TabIndex = 8;
+            this.btnHoanthanh.Text = "Hoàn thành";
+            this.btnHoanthanh.UseVisualStyleBackColor = true;
+            // 
+            // lblEmailgv
+            // 
+            this.lblEmailgv.AutoSize = true;
+            this.lblEmailgv.Location = new System.Drawing.Point(20, 121);
+            this.lblEmailgv.Name = "lblEmailgv";
+            this.lblEmailgv.Size = new System.Drawing.Size(127, 20);
+            this.lblEmailgv.TabIndex = 9;
+            this.lblEmailgv.Text = "Email Giảng viên";
+            // 
+            // lblMota
+            // 
+            this.lblMota.AutoSize = true;
+            this.lblMota.Location = new System.Drawing.Point(20, 273);
+            this.lblMota.Name = "lblMota";
+            this.lblMota.Size = new System.Drawing.Size(101, 20);
+            this.lblMota.TabIndex = 10;
+            this.lblMota.Text = "Mô tả tiến độ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 121);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(473, 26);
+            this.textBox2.TabIndex = 11;
+            // 
+            // rTxtMota
+            // 
+            this.rTxtMota.Location = new System.Drawing.Point(187, 270);
+            this.rTxtMota.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rTxtMota.Name = "rTxtMota";
+            this.rTxtMota.Size = new System.Drawing.Size(473, 225);
+            this.rTxtMota.TabIndex = 12;
+            this.rTxtMota.Text = "";
+            // 
+            // FTienDo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 584);
+            this.Controls.Add(this.rTxtMota);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblMota);
+            this.Controls.Add(this.lblEmailgv);
+            this.Controls.Add(this.btnHoanthanh);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblThoigiancl);
+            this.Controls.Add(this.TxtTenluanvan);
+            this.Controls.Add(this.lblLuanvan);
+            this.Controls.Add(this.lblTiendo);
+            this.Controls.Add(this.progressBar1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FTienDo";
+            this.Text = "Tiến_độ";
+            this.Load += new System.EventHandler(this.Tiến_độ_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblTiendo;
+        private System.Windows.Forms.Label lblLuanvan;
+        private System.Windows.Forms.TextBox TxtTenluanvan;
+        private System.Windows.Forms.Label lblThoigiancl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnHoanthanh;
+        private System.Windows.Forms.Label lblEmailgv;
+        private System.Windows.Forms.Label lblMota;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox rTxtMota;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ TienDo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_Li_Luan_Van
+{
+    public partial class FTienDo : Form
+    {
+        public FTienDo()
+        {
+            InitializeComponent();
+        }
+
+        private void Tiến_độ_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
