624bb024312a2484308ebc89a552484a17b6594d
HaiAnh251 <149868011+HaiAnh251@users.noreply.github.com>
2024-03-19 15:09:40 +0700

    .

---
+++ FThemluanvan.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Quan_Li_Luan_Van
+{
+    partial class FThemluanvan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTenluanvan = new System.Windows.Forms.Label();
+            this.lblMaluanvan = new System.Windows.Forms.Label();
+            this.lblTRangthai = new System.Windows.Forms.Label();
+            this.lblMota = new System.Windows.Forms.Label();
+            this.rdbChuahoanthanh = new System.Windows.Forms.RadioButton();
+            this.rdbDahoanthanh = new System.Windows.Forms.RadioButton();
+            this.txtTenluanvan = new System.Windows.Forms.TextBox();
+            this.txtMaluanvan = new System.Windows.Forms.TextBox();
+            this.rtxtMota = new System.Windows.Forms.RichTextBox();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTenluanvan
+            // 
+            this.lblTenluanvan.AutoSize = true;
+            this.lblTenluanvan.Location = new System.Drawing.Point(37, 31);
+            this.lblTenluanvan.Name = "lblTenluanvan";
+            this.lblTenluanvan.Size = new System.Drawing.Size(84, 16);
+            this.lblTenluanvan.TabIndex = 0;
+            this.lblTenluanvan.Text = "Tên luận văn";
+            // 
+            // lblMaluanvan
+            // 
+            this.lblMaluanvan.AutoSize = true;
+            this.lblMaluanvan.Location = new System.Drawing.Point(40, 106);
+            this.lblMaluanvan.Name = "lblMaluanvan";
+            this.lblMaluanvan.Size = new System.Drawing.Size(79, 16);
+            this.lblMaluanvan.TabIndex = 1;
+            this.lblMaluanvan.Text = "Mã luận văn";
+            // 
+            // lblTRangthai
+            // 
+            this.lblTRangthai.AutoSize = true;
+            this.lblTRangthai.Location = new System.Drawing.Point(40, 198);
+            this.lblTRangthai.Name = "lblTRangthai";
+            this.lblTRangthai.Size = new System.Drawing.Size(67, 16);
+            this.lblTRangthai.TabIndex = 2;
+            this.lblTRangthai.Text = "Trạng thái";
+            // 
+            // lblMota
+            // 
+            this.lblMota.AutoSize = true;
+            this.lblMota.Location = new System.Drawing.Point(40, 270);
+            this.lblMota.Name = "lblMota";
+            this.lblMota.Size = new System.Drawing.Size(40, 16);
+            this.lblMota.TabIndex = 3;
+            this.lblMota.Text = "Mô tả";
+            // 
+            // rdbChuahoanthanh
+            // 
+            this.rdbChuahoanthanh.AutoSize = true;
+            this.rdbChuahoanthanh.Location = new System.Drawing.Point(158, 194);
+            this.rdbChuahoanthanh.Name = "rdbChuahoanthanh";
+            this.rdbChuahoanthanh.Size = new System.Drawing.Size(127, 20);
+            this.rdbChuahoanthanh.TabIndex = 4;
+            this.rdbChuahoanthanh.TabStop = true;
+            this.rdbChuahoanthanh.Text = "Chưa hoàn thành";
+            this.rdbChuahoanthanh.UseVisualStyleBackColor = true;
+            // 
+            // rdbDahoanthanh
+            // 
+            this.rdbDahoanthanh.AutoSize = true;
+            this.rdbDahoanthanh.Location = new System.Drawing.Point(409, 196);
+            this.rdbDahoanthanh.Name = "rdbDahoanthanh";
+            this.rdbDahoanthanh.Size = new System.Drawing.Size(113, 20);
+            this.rdbDahoanthanh.TabIndex = 5;
+            this.rdbDahoanthanh.TabStop = true;
+            this.rdbDahoanthanh.Text = "Đã hoàn thành";
+            this.rdbDahoanthanh.UseVisualStyleBackColor = true;
+            // 
+            // txtTenluanvan
+            // 
+            this.txtTenluanvan.BackColor = System.Drawing.SystemColors.Control;
+            this.txtTenluanvan.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTenluanvan.Location = new System.Drawing.Point(40, 67);
+            this.txtTenluanvan.Name = "txtTenluanvan";
+            this.txtTenluanvan.Size = new System.Drawing.Size(491, 15);
+            this.txtTenluanvan.TabIndex = 6;
+            // 
+            // txtMaluanvan
+            // 
+            this.txtMaluanvan.BackColor = System.Drawing.SystemColors.Control;
+            this.txtMaluanvan.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMaluanvan.Location = new System.Drawing.Point(43, 148);
+            this.txtMaluanvan.Name = "txtMaluanvan";
+            this.txtMaluanvan.Size = new System.Drawing.Size(488, 15);
+            this.txtMaluanvan.TabIndex = 7;
+            this.txtMaluanvan.TextChanged += new System.EventHandler(this.txtMaluanvan_TextChanged);
+            // 
+            // rtxtMota
+            // 
+            this.rtxtMota.Location = new System.Drawing.Point(115, 270);
+            this.rtxtMota.Name = "rtxtMota";
+            this.rtxtMota.Size = new System.Drawing.Size(453, 126);
+            this.rtxtMota.TabIndex = 8;
+            this.rtxtMota.Text = "";
+            // 
+            // btnluu
+            // 
+            this.btnluu.Location = new System.Drawing.Point(268, 402);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(105, 36);
+            this.btnluu.TabIndex = 9;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(40, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(484, 2);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(43, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(479, 2);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // FThemluanvan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnluu);
+            this.Controls.Add(this.rtxtMota);
+            this.Controls.Add(this.txtMaluanvan);
+            this.Controls.Add(this.txtTenluanvan);
+            this.Controls.Add(this.rdbDahoanthanh);
+            this.Controls.Add(this.rdbChuahoanthanh);
+            this.Controls.Add(this.lblMota);
+            this.Controls.Add(this.lblTRangthai);
+            this.Controls.Add(this.lblMaluanvan);
+            this.Controls.Add(this.lblTenluanvan);
+            this.Name = "FThemluanvan";
+            this.Text = "FThemluanvan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTenluanvan;
+        private System.Windows.Forms.Label lblMaluanvan;
+        private System.Windows.Forms.Label lblTRangthai;
+        private System.Windows.Forms.Label lblMota;
+        private System.Windows.Forms.RadioButton rdbChuahoanthanh;
+        private System.Windows.Forms.RadioButton rdbDahoanthanh;
+        private System.Windows.Forms.TextBox txtTenluanvan;
+        private System.Windows.Forms.TextBox txtMaluanvan;
+        private System.Windows.Forms.RichTextBox rtxtMota;
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
