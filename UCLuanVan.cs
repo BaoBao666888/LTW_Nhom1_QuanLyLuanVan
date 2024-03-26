7a18f1058adbb29850d285e591ea18a6439c9439
Chiến Phạm <22110116@student.hcmute.edu.vn>
2024-03-26 10:23:43 +0700

    sửa

--- UCLuanVan.cs
+++ UCLuanVan.cs
@@ -17,39 +17,9 @@ namespace Quan_Li_Luan_Van
         {
             InitializeComponent();
         }
-
-        public Label lblId
-        {
-            get { return lbl_id; }
-            set { lbl_id = value; }
-        }
-
-        public Label lblDeTai
-        {
-            get { return lbl_ten; }
-            set { lbl_ten = value; }
-        }
-
-        public Guna2CustomCheckBox cbTrangThai
-        {
-            get { return cb_status; }
-            set { cb_status = value; }
-        }
-
-        public RichTextBox txtMoTa
-        {
-            get { return txt_mota; }
-            set { txt_mota = value; }
-        }
-
         private void UCLuanVan_Load(object sender, EventArgs e)
         {
 
-        }
-
-        public void AnNut()
-        {
-            btn_xoa.Visible = false;
         }
     }
 }
